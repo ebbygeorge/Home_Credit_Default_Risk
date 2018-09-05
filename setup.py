25144a8e2ce3da60074871d9eb52f2dd25b45dec
Ebby George <ebby.george@infostretch.com>
2018-09-05 12:02:51 +0530

    'First'

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' this project was done for the home credit default risk competition organised by kaggle. Using our approach of feature engineering and modeling we were able to achieve a global rank of 772/7198.',
+    author='kerala_blasters',
+    license='',
+)
